2b4ee205999cfa636a58a50a46c626ad0d3b7a96
CruthOver <b3ng.b3ng16@gmail.com>
2017-04-08 12:29:55 +0700

    Update BelahKetupat.java

--- BelahKetupat.java
+++ BelahKetupat.java
@@ -10,23 +10,29 @@ public class BelahKetupat {
         
         for(int i=n; i>=1; i--){
             for(int j=i-1; j>=1; j--){
-                System.out.print(" ");
+                System.out.print("0");
             }
             for(int k=1; k<=((n+1)-i)*2; k++){
                 System.out.print("*");
+            }
+            for(int j=i-1; j>=1; j--){
+                System.out.print("0");
             }
              System.out.println();
         }
         for (int i=1;i<=n;i++){
              for(int j=1;j<=i-1;j++){
-                 System.out.print(" ");
+                 System.out.print("0");
              }
              for(int z=n;z>=i;z--){
               System.out.print("*");
               }
              for(int k=n;k>=i;k--){
                System.out.print("*");
-               }
+             }
+             for(int j=1;j<=i-1;j++){
+                 System.out.print("0");
+             }
              System.out.println("");
          }
 
