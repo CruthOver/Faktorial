641aa4f2f04fd482d7f7d40c6fccd5c47418f021
CruthOver <b3ng.b3ng16@gmail.com>
2017-04-08 12:27:36 +0700

    Add files via upload

---
+++ BelahKetupat.java
@@ -0,0 +1,34 @@
+package belahketupat;
+
+import java.util.Scanner;
+
+public class BelahKetupat {
+    public static void main(String[] args) {
+        Scanner show = new Scanner(System.in);
+        System.out.print("Masukkan Angka : ");
+        int n = show.nextInt();
+        
+        for(int i=n; i>=1; i--){
+            for(int j=i-1; j>=1; j--){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=((n+1)-i)*2; k++){
+                System.out.print("*");
+            }
+             System.out.println();
+        }
+        for (int i=1;i<=n;i++){
+             for(int j=1;j<=i-1;j++){
+                 System.out.print(" ");
+             }
+             for(int z=n;z>=i;z--){
+              System.out.print("*");
+              }
+             for(int k=n;k>=i;k--){
+               System.out.print("*");
+               }
+             System.out.println("");
+         }
+
+    }
+}
